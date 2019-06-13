c95a55a9abb33e7d26842a75aad338699a08fb02
lijiaocn <lijiaocn@foxmail.com>
2019-06-13 16:29:33 +0800

    add idle timeout

--- docker-webshell/Server/main.go
+++ docker-webshell/Server/main.go
@@ -1,9 +1,12 @@
-//create: 2015/04/28 11:51:41 change: 2017/09/06 15:45:21 author:lijiao
+//create: 2015/04/28 11:51:41 Change: 2019/06/13 16:02:02 author:lijiao
 package main
 
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -12,6 +15,10 @@ func main() {
 	mux.Handle("/", Shell{})
 	mux.Handle("/ping", Ping{})
 	mux.Handle("/cookie", Cookie{})
-	server := http.Server{Addr: ":8080", Handler: mux}
+	idleTimeout, err := strconv.ParseInt(os.Getenv("IDLE_TIMEOUT"), 10, 64)
+	if err != nil {
+		panic("IDLE_TIMEOUT not interger")
+	}
+	server := http.Server{Addr: ":8080", Handler: mux, IdleTimeout: time.Duration(idleTimeout) * time.Second}
 	log.Fatal(server.ListenAndServe())
 }
