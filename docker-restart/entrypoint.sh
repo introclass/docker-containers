93c84326729f3ce389601f6c1a21dda92428f978
lijiaocn <lijiaocn@foxmail.com>
2019-10-11 17:53:44 +0800

    add docker-restart/

---
+++ docker-restart/entrypoint.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+#
+# entrypoint.sh
+# Copyright (C) 2019 lijiaocn <lijiaocn@foxmail.com wechat:lijiaocn>
+#
+# Distributed under terms of the GPL license.
+#
+
+echo "start @`date`"
+
+sleep $INTERNAL
+
+echo "exit 1 @`date`"
+exit 1
